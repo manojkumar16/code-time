07ef28ab2716f784ac47662414937029038a1830
m0k00i6 <manoj.kumar0@walmartlabs.com>
2019-08-25 21:24:36 -0500

    print longest common subsequence

--- src/main/java/algo/ds/dp/LCSProblem.java
+++ src/main/java/algo/ds/dp/LCSProblem.java
@@ -1,10 +1,9 @@
 package algo.ds.dp;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Longest Common Subsequence
+ * 
+ * https://www.geeksforgeeks.org/printing-longest-common-subsequence/
  * 
  * https://www.youtube.com/watch?v=NnD96abizww
  * https://www.youtube.com/watch?v=sSno9rV8Rhg
@@ -16,18 +15,18 @@ import java.util.List;
  * “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc are subsequences of “abcdefg”. So a string of length n has 2^n different
  * possible subsequences.
  * 
- * @author mkprasad
+ * @author manoj
  * 
  */
 public class LCSProblem {
-    static String X = "ABACEB";
+    static String X = "abcdaf";
+    static String Y = "acbcf";
 
-    static String Y = "ADBAVCEB";
-
+//    static String X = "ABACEB";
+//    static String Y = "ADBAVCEB";
+    
     static int lcs[][] = new int[X.length() + 1][Y.length() + 1];
 
-    
-    
     public static void main( String[] args ) {
         LCS();
         System.out.println( "length of longest common subsequence using [Memoization]: "+ lcs[X.length()][Y.length()] );
@@ -35,6 +34,8 @@ public class LCSProblem {
         
         int max = lcsRecursion();
         System.out.println("length of longest common subsequence using [Recursion]: "+max);
+        
+        printSubsequence();
     }
 
     /**
@@ -57,7 +58,6 @@ public class LCSProblem {
 	}
 
 	private static void LCS() {
-        List<Character> ls = new ArrayList<Character>();
         for ( int i = 0; i <= X.length(); i++ ) {
             lcs[i][0] = 0;
         }
@@ -69,18 +69,33 @@ public class LCSProblem {
             for ( int j = 1; j <= Y.length(); j++ ) {
                 if ( X.charAt( i - 1 ) == Y.charAt( j - 1 ) ) {
                     lcs[i][j] = 1 + lcs[i - 1][j - 1];
-                    ls.add( X.charAt( i-1 ) );
                 } else {
-                    lcs[i][j] = max( lcs[i - 1][j], lcs[i][j - 1] );
+                    lcs[i][j] = Math.max( lcs[i - 1][j], lcs[i][j - 1] );
                 }
             }
         }
         
-        System.out.println(ls);
     }
+	
+	private static void printSubsequence() {
+		// Print subsequence
+		int index = lcs[X.length()][Y.length()];
+		// Create a character array to store the lcs string
+		char[] cs = new char[index];
+		int i = X.length(), j = Y.length();
+		while (i > 0 && j > 0) {
+			if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+				cs[index - 1] = X.charAt(i - 1);
+				i--;
+				j--;
+				index--;
+			} else if (lcs[i - 1][j] < lcs[i][j - 1]) {
+				j--;
+			} else {
+				i--;
+			}
+		}
 
-    private static int max( int i, int j ) {
-        return i > j ? i : j;
-    }
-
+		System.out.println("Longest Common Subsequence is: "+new String(cs));
+	}
 }
