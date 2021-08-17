499bc7058364b2068d5be648392d591c619463d6
manojkumar16 <manojkumar16@gmail.com>
2021-08-16 22:02:21 -0500

    ListNode helper class

--- src/main/java/algo/ds/leetcode/ListNode.java
+++ src/main/java/algo/ds/leetcode/ListNode.java
@@ -1,1 +1,20 @@
+package algo.ds.leetcode;
 
+public class ListNode {
+	public int val;
+	public ListNode next;
+
+	public ListNode(int x) {
+		val = x;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder("" + val);
+		ListNode temp = next;
+		while (temp != null) {
+			sb.append(" -> " + temp.val);
+			temp = temp.next;
+		}
+		return sb.toString();
+	}
+}
